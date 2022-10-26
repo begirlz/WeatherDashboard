6d3dec16057bc96d418096213675f8729d3a7689
Supapun Wichachuai <Supapun.Wichachuai@Microchip.com>
2022-10-26 10:04:29 -0700

    clean up files

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,9 @@ var todayDate = moment().format('L');
 const kevlinToFahrenheit = tempKel => tempKel - 255.372; // -255.372 kelvin = 0 deg Fahrenheit
 
 // Display 5 days of data
-function FiveDayWeather(currentCity, lat, lon) {
-    var city = currentCity.split(',');
-    currentCityinfo.text(city[0] + ',' + city[1] + ', ' + todayDate);
+function FiveDayWeather(selectedCity, strLat, strLon) {
 
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily' + '&appid=' + apiKey;
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + strLat + '&lon=' + strLon + '&exclude=hourly,daily' + '&appid=' + apiKey;
 
     fetch(weatherUrl)
         .then(function (resp) {
@@ -31,71 +29,51 @@ function FiveDayWeather(currentCity, lat, lon) {
 
             var tDate = moment(new Date(todayDate)).format('DD');
 
-                var id =1;
-                
-                for (var i = 0; i < dataList.length; i++) {
-                    var nextDate = moment(new Date(dataList[i].dt_txt)).format('DD');                       
-                    $('#' + id).empty();
+            var id = 1;
+            $('.fiveDay').remove();
+            for (var i = 0; i < dataList.length; i++) {
+                var nextDate = moment(new Date(dataList[i].dt_txt)).format('DD');
 
-                    if (nextDate !== tDate) {
+                if (nextDate !== tDate) {
 
-                        var weatherData = dataList[i].main;
-                        
-                        var windSpeed = dataList[i].wind;
-                        
-                        var dataIcon = dataList[i].weather;
-                        
-                        var iconUrl = 'http://openweathermap.org/img/wn/' + dataIcon[0].icon + '@2x.png';
-                                                
-                        var temp = Math.round(kevlinToFahrenheit(weatherData.temp)).toFixed(2);
-                        var dayDate = moment(new Date(dataList[i].dt_txt)).format('dddd');
+                    var weatherData = dataList[i].main;
 
-                        $('#' + id).append($('<div>').text(dayDate));
-                        $('#' + id).append($('<img>').attr('src',iconUrl));
-                        $('#' + id).append($('<div>').text('Temp: ' + temp + '\u00B0 F'));
-                        $('#' + id).append($('<div>').text('Wind: ' + windSpeed.speed + ' MPH'));
-                        $('#' + id).append($('<div>').text('Humidity: ' + weatherData.humidity + ' %'));
-                        
-                        tDate = nextDate;
-                        id++;
-                    }
+                    var windSpeed = dataList[i].wind;
 
+                    var dataIcon = dataList[i].weather;
 
+                    var iconUrl = 'http://openweathermap.org/img/wn/' + dataIcon[0].icon + '@2x.png';
+
+                    var temp = Math.round(kevlinToFahrenheit(weatherData.temp)).toFixed(2);
+                    var dayDate = moment(new Date(dataList[i].dt_txt)).format('dddd');
+
+                    $('#' + id).append($('<div>').text(dayDate).attr('class', 'fiveDay'));
+                    $('#' + id).append($('<img>').attr('src', iconUrl).attr('class', 'fiveDay'));
+                    $('#' + id).append($('<div>').text('Temp: ' + temp + '\u00B0 F').attr('class', 'fiveDay'));
+                    $('#' + id).append($('<div>').text('Wind: ' + windSpeed.speed + ' MPH').attr('class', 'fiveDay'));
+                    $('#' + id).append($('<div>').text('Humidity: ' + weatherData.humidity + ' %').attr('class', 'fiveDay'));
+
+                    tDate = nextDate;
+                    id++;
                 }
 
-        })
-        .catch(function () {
-            // catch any errors
-        });
 
-}
-
-
-function getWeatherByGeo(lat, lon) {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&exclude=hourly';
-
-    fetch(weatherUrl)
-        .then(function (resp) {
-            return resp.json()
-        })
-        .then(function (data) {
-
-            for (var i = 0; i <= data.length; i++) {
-                divTodayForcast.append($('<option>').val(i + 1).text(data[i].name + ',' + data[i].state + ',' + data[i].country));
             }
 
         })
         .catch(function () {
             // catch any errors
         });
+
 }
 
-function defaultWeather(currentCity, lat, lon) {
+//Display weather information
+function defaultWeather(selectedCity, strLat, strLon) {
 
-    var city = currentCity.split(',');
+    var city = selectedCity.split(',');
     currentCityinfo.text(city[0] + ',' + city[1] + ', ' + todayDate);
 
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&exclude=hourly';
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + strLat + '&lon=' + strLon + '&appid=' + apiKey;
 
     fetch(weatherUrl)
         .then(function (resp) {
@@ -109,7 +87,7 @@ function defaultWeather(currentCity, lat, lon) {
             var dataIcon = dataList[0].weather;
             var iconUrl = 'http://openweathermap.org/img/wn/' + dataIcon[0].icon + '@2x.png';
             $('#currentIcon').attr('src', iconUrl);
-            //console.log(iconUrl);
+
             var temp = Math.round(kevlinToFahrenheit(weatherData.temp)).toFixed(2);
             $('#currentTemp').text('Temp: ' + temp + '\u00B0 F');
             $('#currentWind').text('Wind: ' + windSpeed.speed + ' MPH');
@@ -121,14 +99,16 @@ function defaultWeather(currentCity, lat, lon) {
                 divTodayForcast.append($('<option>').val(i + 1).text(data[i].name + ',' + data[i].state + ',' + data[i].country));
             }
 
+            FiveDayWeather(currentCity, strLat, strLon);
         })
         .catch(function () {
             // catch any errors
         });
 
 }
-defaultWeather(currentCity, lat, lon);
-FiveDayWeather(currentCity, lat, lon);
+window.onload = function(){
+        defaultWeather(currentCity, lat, lon);
+}
 
 // Populate city name, state and country searching by city name
 function searchGeoByCity(city) {
@@ -138,7 +118,7 @@ function searchGeoByCity(city) {
             return resp.json()
         })
         .then(function (data) {
-            console.log(data);
+
             cityList.children().remove().end();
             cityList.append($('<option disabled selected>').val(0).text('Select your city'));
 
@@ -152,22 +132,23 @@ function searchGeoByCity(city) {
         });
 }
 
-function getGeoByCity(selectedCity) {
+function getGeoByCity(selectedCity, strLat, strLon) {
     var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + '&limit=5&appid=' + apiKey;
     fetch(geoUrl)
         .then(function (resp) {
             return resp.json()
         })
         .then(function (data) {
-            console.log(data);
-            selectedCity = selectedCity.split(',');
-            //console.log(selectedCity);
+
+            var city = selectedCity.split(',');
+
             for (var i = 0; i <= data.length; i++) {
 
-                if (((selectedCity[0]) === data[i].name) && ((selectedCity[1]) === data[i].state) && ((selectedCity[2]) === data[i].country)) {
+                if (((city[0]) === data[i].name) && ((city[1]) === data[i].state) && ((city[2]) === data[i].country)) {
+                    var strLat = data[i].lat;
+                    var strLon = data[i].lon;
 
-                    defaultWeather(selectedCity, data[i].lat, data[i].lon);
-
+                    defaultWeather(selectedCity, strLat, strLon);
                 }
             }
 
@@ -179,44 +160,42 @@ function getGeoByCity(selectedCity) {
 
 // Search button click
 searchBtn.on('click', function () {
+    cityList.empty();
     searchGeoByCity(searchText.val());
-    FiveDayWeather(currentCity, lat, lon);
-    
 });
 
 // Selecct change status
 cityList.on('change', function (e) {
     var text = $(this).find('option:selected').text();
-
     getGeoByCity(text);
-    defaultWeather(text, lat, lon);
-    
-
     addToLocalStorage(text);
-
 });
 
+// Display previously search results
+$(document).on('click', '#txtPrevSearch', function () {
+   getGeoByCity($(this).val());
+})
 
 // Store search in localStorage
 function addToLocalStorage(text) {
     var id = localStorage.length;
-    localStorage.setItem(id+1, text);
-    $('#prevSearch').append('<input type="button" class="w-100 mb-2" value="' + localStorage.getItem(id+1) + '">');
+    localStorage.setItem(id + 1, text);
+    getLocalStorageData();
 }
-//localStorage.clear();
+
 //Clear localStorage
-$('#clearSearch').on('click', function () {
-    console.log(localStorage.length);
+$(document).on('click', '#clearSearch', function () {
     localStorage.clear();
     $('#prevSearch').empty();
 })
 // Get previously search results
 function getLocalStorageData() {
+    $('#prevSearch').empty();
     if (localStorage.length > 0) {
-        $('#prevSearch').append('<input type="button" class="w-100 mb-2 btn-danger" id="clearSearch" value="Clear Search">');
-        for (var x= 1; x <= localStorage.length; x++) {
-            $('#prevSearch').append('<input type="button" class="w-100 mb-2" value="' + localStorage.getItem(x) + '">');
-            
+        $('#prevSearch').append('<input type="button" class="w-100 mb-2 btn-danger .clearBth" id="clearSearch" value="Clear Search">');
+        for (var x = 1; x <= localStorage.length; x++) {
+            $('#prevSearch').append('<input type="button" id="txtPrevSearch" class="w-100 mb-2" value="' + localStorage.getItem(x) + '">');
+
         }
     }
 }
